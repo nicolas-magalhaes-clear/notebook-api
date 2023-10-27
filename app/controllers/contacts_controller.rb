3c557bb74b58ab4d5d56669961678894570f96b8
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-27 12:51:26 -0300

    update: index return author

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,7 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
 
-    render json: @contacts
+    render json: @contacts, methods: :author, root: true
   end
 
   # GET /contacts/1
