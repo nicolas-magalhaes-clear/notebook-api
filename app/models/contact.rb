2d710badba05ff4e56801c5e7d1db9250d8f6079
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-27 12:51:03 -0300

    added methods to internal data

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,18 @@
 class Contact < ApplicationRecord
+  belongs_to :kind
+  def author
+    "Jacksu"
+  end
+
+  def kind_description
+    self.kind.description
+  end
+
+  def as_json(options={})
+    super(
+      root: true,
+      methods: [:author, :kind_description]
+      )
+  end
+
 end
