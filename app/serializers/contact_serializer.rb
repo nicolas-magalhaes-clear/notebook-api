6eddfbfafedda92ec770db7bef9d060fff16103e
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-30 12:51:28 -0300

    added relation to serializer instead of model

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,10 +1,20 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
-  belongs_to :kind, optional: true
+  belongs_to :kind, optional: true do
+    link(:kind) {kind_url(object.kind.id)}
+  end
   has_many :phones
   has_one :address
 
+
+  link(:self) {contact_url(object.id)}
+
+
+
+  meta do
+    {author: "KKKKKBraia"}
+  end
   def attributes(*args)
     h = super(*args)
     #h[:birthdate] = I18n.l(object.birthdate) unless object.birthdate.blank?
