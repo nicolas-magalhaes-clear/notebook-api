924eb4465bce4a890961a5fd97d14e3e1a48540c
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-31 12:38:37 -0300

    has_many association created between contacts and phones

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,19 +2,24 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
   belongs_to :kind, optional: true do
-    link(:kind) {kind_url(object.kind.id)}
+    link(:related) {contact_kind_url(object.id)}
   end
-  has_many :phones
+
+  has_many :phones do
+    link(:related) { contact_kind_url(object.id) }
+  end
+
   has_one :address
 
 
-  link(:self) {contact_url(object.id)}
+  #link(:self) {contact_url(object.id)}
 
 
 
   meta do
     {author: "KKKKKBraia"}
   end
+
   def attributes(*args)
     h = super(*args)
     #h[:birthdate] = I18n.l(object.birthdate) unless object.birthdate.blank?
